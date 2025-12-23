b348b23b4ff9d2a34c0517a68428e2761e6307b5
mahdi <72223577+mahdi310-u0@users.noreply.github.com>
2025-12-24 02:57:17 +0330

    unzipgit

---
+++ Kq1jOHknT3/end.php
@@ -0,0 +1,6 @@
+<?php
+echo "<body style='background-color: black'>";
+echo "<h1 style='color: red'><center>The end</center></h1></b>";
+echo "<h3 style='color: green'><center>Bravo! Well done...</center></h3>";
+echo "<h3 style='color: green'><center><b>Never</b> take part in a challenge that <b>doesn't</b> offer a prize</center></h3></body>";
+?>
