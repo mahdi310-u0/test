864b7de3c3b6da98384db25cd6bf027583a95630
mahdi <72223577+mahdi310-u0@users.noreply.github.com>
2025-12-24 02:58:33 +0330

    unzipgit

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	
+	<meta name="robots" content="nofollow,noarchive">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" type="image/png" href="/statics/favicon-black.png" media="(prefers-color-scheme: light)">
+	<link rel="icon" type="image/x-icon" href="/statics/favicon-black.ico" media="(prefers-color-scheme: light)">
+	<link rel="icon" type="image/png" href="/statics/favicon-white.png" media="(prefers-color-scheme: dark)">
+	<link rel="icon" type="image/x-icon" href="/statics/favicon-white.ico" media="(prefers-color-scheme: dark)">
+
+	<meta property="og:image" content="/statics/images/splash.jpg">
+	<meta property="og:image:secure_url" content="/statics/images/splash.jpg ">
+	<meta property="og:image:type" content="image/png">
+	<meta property="og:image:width" content="512">
+	<meta property="og:image:height" content="512">
+
+	<meta property="og:type" content="website">
+
+	<meta property="og:title" content="Auroral CTF Challenge">
+	<meta property="og:site_name" content="Auroral CTF Challenge">
+	<meta property="og:description" content="Auroral - Simple fight">
+	<meta name="description" content="Auroral - Simple fight">
+	<meta name="keywords" content="Auroral, CTF, Challenge, fight">
+
+	<link rel="stylesheet" href="/statics/styles/home.css">
+
+	<title>Auroral simple fight</title>
+</head>
+<body>
+	<div class="video">
+		<video poster="/statics/videos/poster.jpg" muted playsinline autoplay loop>
+			<source src="/statics/videos/poster.webm" type="video/webm">
+			<source src="/statics/videos/poster.mp4" type="video/mp4">
+		</video>
+	</div>
+
+	<main class="content home">
+		<h1>Auroral Challenge</h1>
+
+
+<h2>Overview</h2>
+<p>
+The challenge, called <b>Auroral</b>, is designed to increase skills and accuracy. Anyone can participate in this challenge, as this challenge also has an educational purpose and it is compiled in 8 stages.
+</p>
+<p>
+
+<h2>Knowledge</h2>
+<p>
+This challenge requires a basic understanding of networks, web applications, HTTP, programming concepts, a basic understanding of security, effective use of search engines such as Google, and critical thinking. It is very important to approach each step with attention to detail.
+
+</p>
+<p>
+Remember to carefully examine the puzzle pieces provided to successfully solve each step. The challenge steps do not require a brute force attack or automatic phasing of the HTTP protocol. However, some steps may involve an offline brute force attack. Please refrain from phasing as it may result in a permanent IP ban. During the challenge, you can use Google, ChatGPT and other online resources for help.<br><b>Note</b>: The steps are not ordered from easy to difficult.
+</p>
+
+<h2>Timeline</h2>
+<p>
+The challenge started on December 23rd and will be available for 7 days until December 30th.
+</p>
+
+<br>
+<br>
+
+<p>You can start the challenge from <a href="<?echo 'http://'.$_SERVER['HTTP_HOST'].'/level-01';?>">Level 01</a></p>
+</main>
+
+	
+<h3>Copyright: The original site template is taken from the Arrival challenge.</h3>
+</body></html>
