8a8f37719bd32f05a61cfa3f5e07b6bdaf3cd48f
mahdi <72223577+mahdi310-u0@users.noreply.github.com>
2025-12-24 02:57:04 +0330

    unzipgit

---
+++ B7R5he7kqP/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="robots" content="noindex,nofollow,noarchive">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="/statics/favicon/black.png" media="(prefers-color-scheme: light)">
+    <link rel="icon" type="image/x-icon" href="/statics/favicon/black.ico" media="(prefers-color-scheme: light)">
+    <link rel="icon" type="image/png" href="/statics/favicon/white.png" media="(prefers-color-scheme: dark)">
+    <link rel="icon" type="image/x-icon" href="/statics/favicon/white.ico" media="(prefers-color-scheme: dark)">
+
+
+    <link rel="stylesheet" href="/statics/styles/levels.css">
+    <title>
+      Auroral | Level 06
+    </title>
+  </head>
+  <body>
+    <div class="image">
+      <picture><source srcset="/statics/images/Pqk7eh5R7B.webp" type="image/webp"> <source srcset="/statics/images/Pqk7eh5R7B.jpg" type="image/jpeg"> <img src="/statics/images/Pqk7eh5R7B.jpg" alt="Background"></picture>
+    </div>
+
+    <main class="content level">
+
+      <h1>
+        Level <b>06</b>
+      </h1>
+
+      <p>
+        Everything must be examined, even symbols and emblems.
+      </p>
+    </main>
+    <!-- Google tag (gtag.js) -->
+     
+    
+  
+</body></html>
