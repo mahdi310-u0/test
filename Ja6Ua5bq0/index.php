16bd9d2143174d26ea785776c774add31d7749fc
mahdi <72223577+mahdi310-u0@users.noreply.github.com>
2025-12-24 02:57:11 +0330

    unzipgit

---
+++ Ja6Ua5bq0/index.php
@@ -0,0 +1,25 @@
+<?php
+// ۱. ارسال هدر ۴۰۴ واقعی به مرورگر
+header("HTTP/1.1 404 Not Found");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404 Not Found</title>
+</head>
+<body>
+    <iframe src="/Bravo? Next level: /Kq1jOHknT3"></iframe>
+</body>
+  <style>
+        body, html { margin: 0; padding: 0; height: 100%; overflow: hidden; }
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+            position: absolute;
+            top: 0;
+            left: 0;
+        }
+    </style>
+</html>
