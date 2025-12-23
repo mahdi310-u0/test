0acd433ad916bc334e0723e74897bd374efc6b14
mahdi <72223577+mahdi310-u0@users.noreply.github.com>
2025-12-24 02:57:18 +0330

    unzipgit

---
+++ level-01/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <meta name="robots" content="noindex,nofollow,noarchive">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="/statics/favicon-black.png" media="(prefers-color-scheme: light)">
+    <link rel="icon" type="image/x-icon" href="/statics/favicon-black.ico" media="(prefers-color-scheme: light)">
+    <link rel="icon" type="image/png" href="/statics/favicon-white.png" media="(prefers-color-scheme: dark)">
+    <link rel="icon" type="image/x-icon" href="/statics/favicon-white.ico" media="(prefers-color-scheme: dark)">
+
+
+    <link rel="stylesheet" href="/statics/styles/levels.css">
+    <link rel="stylesheet" href="/static/styles/styles.css">
+    <title>
+      Auroral | Level 01
+    </title>
+  </head>
+  <body>
+    <div class="image">
+      <picture><source srcset="/statics/images/10-level.webp" type="image/webp"> <source srcset="/statics/images/10-level.jpg" type="image/jpeg"> <img src="/statics/images/10-level.jpg" alt="Background"></picture>
+    </div>
+
+    <main class="content level">
+
+      <h1>
+        Level <b>01</b>
+      </h1>
+
+      <p>
+        Resources?
+      </p>
+    </main>
+    <!-- Google tag (gtag.js) -->
+     
+    
+  
+</body></html>
